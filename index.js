abfd344b8aa4d55b7db73a3f0cb499eafd2e4b24
davidnmora <davidnmora@gmail.com>
2019-12-11 15:05:35 -0500

    First pass at Class-ifying viz :package:

--- index.js
+++ index.js
@@ -25,77 +25,41 @@ const PATH_DRAWING_ANIMATION_DURATION = 3000
 const circleNotesData = Object.values(circleNotesDataByNote)
 const colorScale = d3.scaleLinear().domain([0, circleNotesData.length]).range(['blue', 'red'])
 const height = 500, width = 500
-const svg = d3.select('#canvas').append('svg').attr('width', 900).attr('height', 900).style('background-color', colors.canvas)
-const songPathGenerator = d3.line()
-	.curve(d3.curveLinear)
-	.x(chord => getChordXY(chord).x)
-	.y(chord => getChordXY(chord).y);
-
-function getChordXY(chord) {
-	const [x, y] = getCoordsFromIndex(circleNotesDataByNote[chord.root][indexType], true)
-	return {x, y}
-}
-
-// Globals
-let indexType = 'fifthsIndex'
-let noteGroup, songPath;
 
 
-
-const CHORD_ARRAY = CHORD_PROGRESSIONS.AUTUMN_LEAVES
-
-
-// Helper functions:
-
-
-// Coord Calculations
-function getCoordsFromIndex(index, jitter = false) {
-	const increment = 2 * Math.PI / circleNotesData.length
-	const angle = increment * index
-	const radius = 200
-	const [x, y] = d3.pointRadial(angle, radius)
-	const _jitter = jitter ? Math.random() * 16 : 0
-	return [x + width/2 + _jitter, y + height/2 + _jitter]
-}
-
-function updateCircleNotesData(circleNotesData) {
-	circleNotesData.forEach(d => {
-		const [x, y] = getCoordsFromIndex(d[indexType])
-		d.x = x
-		d.y = y
-		d.color = colorScale(d['chromaticIndex']) // Keep color constant regardless of fifths or chromatic
-	})
-}
-
-
-// Main function
-function updateVis() {
-	indexType = indexType === 'chromaticIndex' ? 'fifthsIndex' : 'chromaticIndex'
-	d3.select('#circle-type').html(indexType)
-	
-	updateCircleNotesData(circleNotesData, indexType)
-	// Create high-level vis features if needed
-	if (!noteGroup) {
-		noteGroup = svg.selectAll('.note-group')
+class ChordViz extends Object {
+	constructor(CHORD_ARRAY, visContainer) {
+		super()
+		console.log('CONSTRUCTOR')
+		this.CHORD_ARRAY = CHORD_ARRAY
+		this.svg = d3.select(visContainer).append('svg').attr('width', 900).attr('height', 900).style('background-color', colors.canvas)
+		this.indexType = 'fifthsIndex'
+		d3.select('#circle-type').html(this.indexType).on('click', () => {
+			this.toggleCircle()
+		})
+		
+		this.updateCircleNotesData()
+		
+		
+		this.noteGroup = this.svg.selectAll('.note-group')
 			.data(circleNotesData).enter()
 			.append("g")
 			.classed('note-group', true)
 			.attr("transform", d => `translate(${d.x},${d.y})`)
 		
-		const noteCircle = noteGroup
+		const noteCircle = this.noteGroup
 			.append("circle")
 			.attr("r", d => d.note === 'E' ? 20 : 0)
 			.attr("fill", d => d.color)
 		
-		const noteLabel = noteGroup
+		const noteLabel = this.noteGroup
 			.append("text")
 			.attr('font-size', 46)
 			.attr('fill', colors.text)
 			.text(d => d.note.replace('sharp', '#').replace('flat', 'b'))
-	}
-	
-	if (!songPath) {
-		songPath = svg.append('path')
+		
+		
+		this.songPath = this.svg.append('path')
 			.classed('song-path', true)
 			.attr('fill', 'none')
 			.attr('stroke', colors.line)
@@ -104,60 +68,82 @@ function updateVis() {
 			.style('stroke-linecap','round')
 			.style("stroke-linejoin","round")
 		
-		const s = songPathGenerator(CHORD_ARRAY)
-		songPath
+		const s = this.songPathGenerator(CHORD_ARRAY)
+		this.songPath
 			.attr('d', s)
 	}
 	
-	// Transition to updated state
-	noteGroup.transition().duration(CIRCLE_OF_FOURTHS_TRANSITION_DURATION)
-	// .delay((d, i) => i * 50) // To coordinate w/ songPath, we'd need to transition to a NEW line for each point in the line
-		.attr("transform", d => `translate(${d.x},${d.y})`)
+	songPathGenerator = d3.line()
+		.curve(d3.curveLinear)
+		.x(chord => this.getChordXY(chord).x)
+		.y(chord => this.getChordXY(chord).y);
 	
 	
-	// If paths exists, transition it to this new state.
-	songPath
-		.transition()
-		.duration(CIRCLE_OF_FOURTHS_TRANSITION_DURATION)
-		.attrTween('d', function (d) { // SOURCE/PLUGIN: https://github.com/pbeshai/d3-interpolate-path
-			var previous = d3.select(this).attr('d');
-			var current = songPathGenerator(CHORD_ARRAY);
-			
-			return d3.interpolatePath(previous, current);
+	getChordXY(chord) {
+		const [x, y] = this.getCoordsFromIndex(circleNotesDataByNote[chord.root][this.indexType], true)
+		return {x, y}
+	}
+
+// Coord Calculations
+	getCoordsFromIndex(index, jitter = false) {
+		const increment = 2 * Math.PI / circleNotesData.length
+		const angle = increment * index
+		const radius = 200
+		const [x, y] = d3.pointRadial(angle, radius)
+		const _jitter = jitter ? Math.random() * 16 : 0
+		return [x + width/2 + _jitter, y + height/2 + _jitter]
+	}
+	
+	updateCircleNotesData() {
+		circleNotesData.forEach(d => {
+			const [x, y] = this.getCoordsFromIndex(d[this.indexType])
+			d.x = x
+			d.y = y
+			d.color = colorScale(d['chromaticIndex']) // Keep color constant regardless of fifths or chromatic
 		})
-		.on('end', _ => runPathDrawingAnimation(songPath))
+	}
+	
+	runPathDrawingAnimation() {
+		// https://observablehq.com/@lemonnish/svg-path-animations-d3-transition
+		var totalLength = this.songPath.node().getTotalLength();
 		
-	
-
-}
-
-function runPathDrawingAnimation(songPath) {
-	// https://observablehq.com/@lemonnish/svg-path-animations-d3-transition
-	var totalLength = songPath.node().getTotalLength();
-	
-	songPath
-		.attr("stroke-dasharray", totalLength + " " + totalLength)
-		.attr("stroke-dashoffset", totalLength)
-		.transition()
-		.duration(PATH_DRAWING_ANIMATION_DURATION)
-		.ease(d3.easeLinear)
-		.attr("stroke-dashoffset", 0)
-}
-
-updateVis()
-
-
-class ChordViz extends Object {
-	constructor(CHORD_ARRAY, containerSelector) {
-		super()
-		console.log('CONSTRUCTOR')
+		this.songPath
+			.attr("stroke-dasharray", totalLength + " " + totalLength)
+			.attr("stroke-dashoffset", totalLength)
+			.transition()
+			.duration(PATH_DRAWING_ANIMATION_DURATION)
+			.ease(d3.easeLinear)
+			.attr("stroke-dashoffset", 0)
 	}
 	
 	toggleCircle() {
 		console.log('TOGGLING')
+		
+		this.indexType = this.indexType === 'chromaticIndex' ? 'fifthsIndex' : 'chromaticIndex'
+		d3.select('#circle-type').html(this.indexType)
+		
+		// Transition to updated state
+		this.noteGroup.transition().duration(CIRCLE_OF_FOURTHS_TRANSITION_DURATION)
+		// .delay((d, i) => i * 50) // To coordinate w/ songPath, we'd need to transition to a NEW line for each point in the line
+			.attr("transform", d => `translate(${d.x},${d.y})`)
+		
+		
+		// If paths exists, transition it to this new state.
+		const songPathGeneratorRef = this.songPathGenerator
+		const CHORD_ARRAY_ref = this.CHORD_ARRAY
+		this.songPath
+			.transition()
+			.duration(CIRCLE_OF_FOURTHS_TRANSITION_DURATION)
+			.attrTween('d', function (d) { // SOURCE/PLUGIN: https://github.com/pbeshai/d3-interpolate-path
+				var previous = d3.select(this).attr('d');
+				var current = songPathGeneratorRef(CHORD_ARRAY_ref);
+				
+				return d3.interpolatePath(previous, current);
+			})
+			.on('end', _ => this.runPathDrawingAnimation())
 	}
 }
 
-const viz = new ChordViz('suppp')
+const viz = new ChordViz(CHORD_PROGRESSIONS.AUTUMN_LEAVES, '#vis-container')
 
 viz.toggleCircle()
